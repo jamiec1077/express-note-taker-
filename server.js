eeac8ee54d0a454c155158de1da0ec468a80caa7
jamiec1077 <jamie.charlemagne1992@gmail.com>
2023-05-20 09:32:46 -0400

    updated spelling error my style.css was misspelled and mispelling in my path

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-const notesFilePath = path.join(__dirname, "db", "notes.json");
+const notesFilePath = path.join(__dirname, "db", "db.json");
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
